0e12583c99257c8c2fbc78612d82774e20b55984
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-23 13:15:43 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// Ce fichier fait le lien entre votre HTML (balise #root) et votre code React (App.jsx)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
