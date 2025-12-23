4cfa9bdae566e5144d670f38494e9d0c594e84d3
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-23 17:39:48 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.ts'
 
 // Ce fichier fait le lien entre votre HTML (balise #root) et votre code React (App.jsx)
 ReactDOM.createRoot(document.getElementById('root')).render(
