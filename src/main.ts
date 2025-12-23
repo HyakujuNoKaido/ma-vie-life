38d8be88f96b9a1ad2098a36586edb16bde0c431
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-23 17:44:43 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import App from './app'; // Importe le 'export default' de votre fichier app.ts
+import App from './app.ts'; // Importe le 'export default' de votre fichier app.ts
 
 bootstrapApplication(App).catch((err) => console.error(err));
