2e3caa97092ceefcce310c93e4bc3340ffacf36f
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-23 17:50:41 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import 'zone.js'; // Obligatoire pour Angular standard
 import { bootstrapApplication } from '@angular/platform-browser';
-import App from './app.ts'; // Importe le 'export default' de votre fichier app.ts
+import { App } from './app'; // Notez les accolades {} car c'est un export nommé maintenant
 
 bootstrapApplication(App).catch((err) => console.error(err));
+
+
