b32373e75de911519d36f92440684a7aa38712fe
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-24 16:35:37 +0100

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// Fichier : src/environments/environment.ts
+// Utilisé pour le développement local sur votre ordinateur
+
+export const environment = {
+  production: false,
+  firebase: {
+    // Clés réelles pour que l'app fonctionne sur votre PC
+    apiKey: "AIzaSyDQlNuPgVI13Jyx1h9ykM7B_6krxltlN6w",
+    authDomain: "mondashboardlife.firebaseapp.com",
+    projectId: "mondashboardlife",
+    storageBucket: "mondashboardlife.firebasestorage.app",
+    messagingSenderId: "361243061610",
+    appId: "1:361243061610:web:ec830caf5da084effec913"
+  }
+};
