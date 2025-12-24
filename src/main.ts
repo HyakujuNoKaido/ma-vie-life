37497439db1baebdb41122a400d5cebf472395d7
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-24 16:21:41 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-import 'zone.js';
-import '@angular/compiler'; // <-- LA SOLUTION MAGIQUE (Roue de secours JIT)
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app';
+import { App } from './app/app.component';
+import { config } from './app/app.config';
 
-bootstrapApplication(App).catch((err) => {
-  console.error("Erreur critique au démarrage :", err);
-  document.body.innerHTML = `
-    <div style="background-color: #fef2f2; color: #991b1b; padding: 20px; font-family: sans-serif;">
-      <h1>⚠️ Erreur au lancement</h1>
-      <pre>${err.message || err}</pre>
-    </div>
-  `;
-});
-
-
+bootstrapApplication(App, config)
+  .catch((err) => console.error(err));
