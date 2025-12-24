f02c12fdd34d61f2527345cf21fdf58bee3027e1
HyakujuNoKaido <128378173+HyakujuNoKaido@users.noreply.github.com>
2025-12-24 16:36:04 +0100

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+// Fichier : src/environments/environment.prod.ts
+// Ce fichier est un PLACEHOLDER pour la production.
+// Ne mettez PAS vos vraies clés ici si vous utilisez GitHub.
+// Le script set-env.js remplira ces valeurs automatiquement sur Cloudflare.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "", 
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
